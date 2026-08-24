c8a49407bbcfaad222b7f615f0d996f660e8312f
Ankit Pramanik <59945244+ankit98040@users.noreply.github.com>
2026-08-24 16:48:57 +0530

    Update github-oidc.tf

--- github-oidc.tf
+++ github-oidc.tf
@@ -44,7 +44,10 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_repository}:*",
+              "repo:ankit98040/*"
+            ]
           }
         }
       }
