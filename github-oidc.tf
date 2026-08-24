f07b2460aaa5045f601272bf0a230f66b12dff66
Ankit Pramanik <59945244+ankit98040@users.noreply.github.com>
2026-08-24 17:05:50 +0530

    Update github-oidc.tf

--- github-oidc.tf
+++ github-oidc.tf
@@ -28,7 +28,7 @@ resource "aws_iam_openid_connect_provider" "github" {
 
 # IAM Role for GitHub Actions
 resource "aws_iam_role" "github_actions" {
-  name = "github-actions-twin-deploy"
+  name = "github-actions-twin-deploy-v2"
   
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
